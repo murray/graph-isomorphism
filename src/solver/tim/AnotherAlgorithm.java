ede1120ad727a26f2717dd791bba48f020e157bd
Tim Yeung <Tim@Tims-MacBook-Air.local>
2016-03-31 21:21:19 -0400

    I will look for another existing algorithm  so we can compare it with Kents for the final report

---
+++ src/solver/tim/AnotherAlgorithm.java
@@ -0,0 +1,18 @@
+package solver.tim;
+
+import graph.StaticGraph;
+import solvers.GISolver;
+
+public class AnotherAlgorithm implements GISolver {
+
+	@Override
+	public boolean isIsomorphism(StaticGraph g1, StaticGraph g2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	public static void main (String[] args){
+		
+	}
+
+}
